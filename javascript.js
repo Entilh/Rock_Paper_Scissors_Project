23b732c9d3e1529e95aefccda14d219072c89eaf
Entilh <gasterhidehand@gmail.com>
2026-04-17 17:17:22 +0200

    Work Finished

--- javascript.js
+++ javascript.js
@@ -13,6 +13,8 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+  const userChoice = prompt("Chose: 1=Rock, 2=Paper, 3=Scissors");
+
   if (userChoice == 1) {
     return "Rock";
   } else if (userChoice == 2) {
@@ -23,10 +25,88 @@ function getHumanChoice() {
 }
 
 /* console.log("Computer Choice: " + getComputerChoice()); */
+function PlayRound(humanSelection, computerSelection) {
+  if (humanSelection == "Rock") {
+    if (computerSelection == "Rock") {
+      console.log("Egalite");
+      computerScore += 1;
+      humanScore += 1;
+    } else if (computerSelection == "Paper") {
+      console.log("l'ordi a gagne");
+      computerScore += 1;
+    } else if (computerSelection == "Scissors") {
+      console.log("l'utilisateur a gagne");
+      humanScore += 1;
+    }
+  } else if (humanSelection == "Paper") {
+    if (computerSelection == "Paper") {
+      console.log("Egalite");
+      computerScore += 1;
+      humanScore += 1;
+    } else if (computerSelection == "Scissors") {
+      console.log("l'ordi a gagne");
+      computerScore += 1;
+    } else if (computerSelection == "Rock") {
+      console.log("l'utilisateur a gagne");
+      humanScore += 1;
+    }
+  } else if (humanSelection == "Scissors") {
+    if (computerSelection == "Scissors") {
+      console.log("Egalite");
+      computerScore += 1;
+      humanScore += 1;
+    } else if (computerSelection == "Rock") {
+      console.log("l'ordi a gagne");
+      computerScore += 1;
+    } else if (computerSelection == "Paper") {
+      console.log("l'utilisateur a gagne");
+      humanScore += 1;
+    }
+  }
+}
 
-const userChoice = prompt("Chose: 1=Rock, 2=Paper, 3=Scissors");
+function PlayGame() {
+  /* Round 1 */
+  let computerSelection1 = getComputerChoice();
+  let humanSelection1 = getHumanChoice();
+  PlayRound(humanSelection1, computerSelection1);
+  /* Round 2 */
+  let computerSelection2 = getComputerChoice();
+  let humanSelection2 = getHumanChoice();
+  PlayRound(humanSelection2, computerSelection2);
+  /* Round 3 */
+  let computerSelection3 = getComputerChoice();
+  let humanSelection3 = getHumanChoice();
+  PlayRound(humanSelection3, computerSelection3);
+  /* Round 4 */
+  let computerSelection4 = getComputerChoice();
+  let humanSelection4 = getHumanChoice();
+  PlayRound(humanSelection4, computerSelection4);
+  /* Round 5 */
+  let computerSelection5 = getComputerChoice();
+  let humanSelection5 = getHumanChoice();
+  PlayRound(humanSelection5, computerSelection5);
+}
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
+function AfficherScore(hS, cS) {
+  console.log("le score de l'utilisateur est: " + hS);
+  console.log("le score de l'ordinateur est: " + cS);
+}
 
-console.log("l'utilisateur a choisit: " + humanChoice);
+function afficherResultatFinal(hS, cS) {
+  if (hS > cS) {
+    console.log("Tu as gagne! Youpi!");
+  } else if (hS < cS) {
+    console.log("Tu as perdu! Dommage!");
+  } else if (hS == cS) {
+    console.log("Egalite Parfaite!");
+  }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+AfficherScore(humanScore, computerScore);
+PlayGame();
+AfficherScore(humanScore, computerScore);
+afficherResultatFinal(humanScore, computerScore);
