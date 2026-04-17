375c2518a9cd226912d82a82d39f76a4a53a77dc
Entilh <gasterhidehand@gmail.com>
2026-04-17 15:26:42 +0200

    getComputerChoice function completed

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 console.log("Rock Paper Scissors");
+
+function getComputerChoice() {
+  let randomNumber = Math.random();
+  /* console.log(randomNumber); */
+  if (randomNumber <= 1 / 3) {
+    return "Rock";
+  } else if ((1 / 3 < randomNumber) & (randomNumber <= 2 / 3)) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+console.log("Computer Choice: " + getComputerChoice());
