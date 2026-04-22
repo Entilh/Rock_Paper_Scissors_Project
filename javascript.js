0126e9a3d1de0e8fff5ccfa1d81d35ed0ab585a3
Entilh <gasterhidehand@gmail.com>
2026-04-22 23:39:46 +0200

    Finish the Exercise

--- javascript.js
+++ javascript.js
@@ -1,112 +1,164 @@
 console.log("Rock Paper Scissors");
 
-function getComputerChoice() {
-  let randomNumber = Math.random();
-  /* console.log(randomNumber); */
-  if (randomNumber <= 1 / 3) {
-    return "Rock";
-  } else if ((1 / 3 < randomNumber) & (randomNumber <= 2 / 3)) {
-    return "Paper";
-  } else {
-    return "Scissors";
+const input = document.querySelector(".nbrRounds");
+const bouton = document.querySelector(".validNbrRounds");
+const containerNbrRounds = document.querySelector("#getNbrRounds");
+
+const gameButtons = document.querySelector(".gameButtons");
+const buttonRock = document.querySelector(".Rock");
+const buttonPaper = document.querySelector(".Paper");
+const buttonScissors = document.querySelector(".Scissors");
+
+const NumCurrentRound = document.querySelector("#NumCurrentRound");
+const CurrentPlayerScore = document.querySelector("#CurrentPlayerScore");
+const CurrentComputerScore = document.querySelector("#CurrentComputerScore");
+const PlayerChoice = document.querySelector("#PlayerChoice");
+const ComputerChoice = document.querySelector("#ComputerChoice");
+
+const PlayGround = document.querySelector("#PlayGround");
+const BeforeScore = document.querySelector(".beforeScore");
+
+const FinalResult = document.querySelector(".FinalResult");
+
+let currentRound = 0;
+let nbrRoundsAsked = 0;
+
+const beginTheGameText = document.createElement("div");
+beginTheGameText.classList.add("beginTheGameText");
+beginTheGameText.textContent = "Veuillez appuyer sur le bouton pour commencer";
+const beginTheGameButton = document.createElement("button");
+beginTheGameButton.classList.add("beginTheGameButton");
+beginTheGameButton.textContent = "Commencer";
+
+bouton.addEventListener("click", () => {
+  nbrRoundsAsked = Number(input.value);
+  const containerResultNbrRounds = document.createElement("div");
+  containerResultNbrRounds.classList.add("resultNbrRounds");
+  containerResultNbrRounds.textContent = `Le nombre de rounds choisi est ${nbrRoundsAsked}`;
+  containerNbrRounds.appendChild(containerResultNbrRounds);
+
+  const beginTheGame = document.createElement("div");
+  beginTheGame.classList.add("beginTheGame");
+  BeforeScore.appendChild(beginTheGame);
+  beginTheGame.appendChild(beginTheGameText);
+  beginTheGame.appendChild(beginTheGameButton);
+  NumCurrentRound.textContent = "0";
+  CurrentPlayerScore.textContent = "0";
+  CurrentComputerScore.textContent = "0";
+});
+
+beginTheGameButton.addEventListener("click", () => {
+  currentRound = 0;
+});
+
+buttonRock.addEventListener("click", () => {
+  let playerChoice = "Rock";
+  PlayerChoice.textContent = playerChoice;
+  let computerChoice = getComputerChoice();
+  PlayRound(playerChoice, computerChoice);
+  currentRound++;
+  if (currentRound == nbrRoundsAsked) {
+    printFinalResult();
+  }
+});
+
+buttonPaper.addEventListener("click", () => {
+  let playerChoice = "Paper";
+  PlayerChoice.textContent = playerChoice;
+  let computerChoice = getComputerChoice();
+  PlayRound(playerChoice, computerChoice);
+  currentRound++;
+  if (currentRound == nbrRoundsAsked) {
+    printFinalResult();
+  }
+});
+
+buttonScissors.addEventListener("click", () => {
+  let playerChoice = "Scissors";
+  PlayerChoice.textContent = playerChoice;
+  let computerChoice = getComputerChoice();
+  PlayRound(playerChoice, computerChoice);
+  currentRound++;
+  if (currentRound == nbrRoundsAsked) {
+    printFinalResult();
+  }
+});
+
+function printFinalResult() {
+  const finalResultText = document.createElement("div");
+  finalResultText.classList.add("FinalResultResult");
+  finalResultText.textContent = getResult();
+  FinalResult.appendChild(finalResultText);
+}
+
+function getResult() {
+  let finalPlayerResult = Number(CurrentPlayerScore.textContent);
+  let finalComputerResult = Number(CurrentComputerScore.textContent);
+  if (finalPlayerResult > finalComputerResult) {
+    return `Vous avez gagne ${finalPlayerResult} a ${finalComputerResult}`;
+  } else if (finalPlayerResult < finalComputerResult) {
+    return `Vous avez perdu ${finalComputerResult} a ${finalPlayerResult}`;
+  } else if (finalPlayerResult == finalComputerResult) {
+    return `Egalite a ${finalComputerResult}`;
   }
 }
 
-function getHumanChoice() {
-  const userChoice = prompt("Chose: 1=Rock, 2=Paper, 3=Scissors");
+function incNumCurrentRound() {
+  let formerRoundNumber = Number(NumCurrentRound.textContent);
+  NumCurrentRound.textContent = formerRoundNumber + 1;
+}
 
-  if (userChoice == 1) {
-    return "Rock";
-  } else if (userChoice == 2) {
-    return "Paper";
-  } else if (userChoice == 3) {
-    return "Scissors";
+function incCurrentPlayerScore() {
+  CurrentPlayerScore.textContent = Number(CurrentPlayerScore.textContent) + 1;
+}
+
+function incCurrentComputerScore() {
+  CurrentComputerScore.textContent =
+    Number(CurrentComputerScore.textContent) + 1;
+}
+
+function getComputerChoice() {
+  let randomNumber = Math.random();
+  let ComputerChoiceCalculated = "";
+  if (randomNumber <= 1 / 3) {
+    ComputerChoiceCalculated = "Rock";
+  } else if ((1 / 3 < randomNumber) & (randomNumber <= 2 / 3)) {
+    ComputerChoiceCalculated = "Paper";
+  } else {
+    ComputerChoiceCalculated = "Scissors";
   }
+  ComputerChoice.textContent = ComputerChoiceCalculated;
+  return ComputerChoiceCalculated;
 }
 
 /* console.log("Computer Choice: " + getComputerChoice()); */
 function PlayRound(humanSelection, computerSelection) {
   if (humanSelection == "Rock") {
     if (computerSelection == "Rock") {
-      console.log("Egalite");
-      computerScore += 1;
-      humanScore += 1;
+      incCurrentComputerScore();
+      incCurrentPlayerScore();
     } else if (computerSelection == "Paper") {
-      console.log("l'ordi a gagne");
-      computerScore += 1;
+      incCurrentComputerScore();
     } else if (computerSelection == "Scissors") {
-      console.log("l'utilisateur a gagne");
-      humanScore += 1;
+      incCurrentPlayerScore();
     }
   } else if (humanSelection == "Paper") {
     if (computerSelection == "Paper") {
-      console.log("Egalite");
-      computerScore += 1;
-      humanScore += 1;
+      incCurrentComputerScore();
+      incCurrentPlayerScore();
     } else if (computerSelection == "Scissors") {
-      console.log("l'ordi a gagne");
-      computerScore += 1;
+      incCurrentComputerScore();
     } else if (computerSelection == "Rock") {
-      console.log("l'utilisateur a gagne");
-      humanScore += 1;
+      incCurrentPlayerScore();
     }
   } else if (humanSelection == "Scissors") {
     if (computerSelection == "Scissors") {
-      console.log("Egalite");
-      computerScore += 1;
-      humanScore += 1;
+      incCurrentComputerScore();
+      incCurrentPlayerScore();
     } else if (computerSelection == "Rock") {
-      console.log("l'ordi a gagne");
-      computerScore += 1;
+      incCurrentComputerScore();
     } else if (computerSelection == "Paper") {
-      console.log("l'utilisateur a gagne");
-      humanScore += 1;
+      incCurrentPlayerScore();
     }
   }
 }
-
-function PlayGame() {
-  /* Round 1 */
-  let computerSelection1 = getComputerChoice();
-  let humanSelection1 = getHumanChoice();
-  PlayRound(humanSelection1, computerSelection1);
-  /* Round 2 */
-  let computerSelection2 = getComputerChoice();
-  let humanSelection2 = getHumanChoice();
-  PlayRound(humanSelection2, computerSelection2);
-  /* Round 3 */
-  let computerSelection3 = getComputerChoice();
-  let humanSelection3 = getHumanChoice();
-  PlayRound(humanSelection3, computerSelection3);
-  /* Round 4 */
-  let computerSelection4 = getComputerChoice();
-  let humanSelection4 = getHumanChoice();
-  PlayRound(humanSelection4, computerSelection4);
-  /* Round 5 */
-  let computerSelection5 = getComputerChoice();
-  let humanSelection5 = getHumanChoice();
-  PlayRound(humanSelection5, computerSelection5);
-}
-
-function AfficherScore(hS, cS) {
-  console.log("le score de l'utilisateur est: " + hS);
-  console.log("le score de l'ordinateur est: " + cS);
-}
-
-function afficherResultatFinal(hS, cS) {
-  if (hS > cS) {
-    console.log("Tu as gagne! Youpi!");
-  } else if (hS < cS) {
-    console.log("Tu as perdu! Dommage!");
-  } else if (hS == cS) {
-    console.log("Egalite Parfaite!");
-  }
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-AfficherScore(humanScore, computerScore);
-PlayGame();
-AfficherScore(humanScore, computerScore);
-afficherResultatFinal(humanScore, computerScore);
